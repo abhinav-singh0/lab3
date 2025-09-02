6257249ec5606ec8327a64bdce1ec02ab4ca58f8
Abhinav Singh <ee25mt010@iitdh.ac.in>
2025-09-02 15:31:37 +0530

    suces

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+int main(void) {
+    SYSCTL_RCGCGPIO_R |= 0x20;               // Enable clock to GPIOF
+        while ((SYSCTL_PRGPIO_R & 0x20) == 0);   // Wait until ready
+
+        GPIO_PORTF_LOCK_R = 0x4C4F434B;          // Unlock commit register
+        GPIO_PORTF_CR_R |= 0x1F;                 // Allow changes to PF0–PF4
+        GPIO_PORTF_DIR_R |= 0x0E;                // PF1, PF2, PF3 = output (LEDs)
+        GPIO_PORTF_DEN_R |= 0x1F;                // Enable digital for PF0–PF4
+        GPIO_PORTF_PUR_R |= 0x11;                // Enable pull-ups on PF0, PF4 (switches)
+
+    while(1) {
+        if((GPIO_PORTF_DATA_R & 0x01) == 0) {   // SW2 pressed (PF0 low)
+            GPIO_PORTF_DATA_R =0x02;
+        }
+        if((GPIO_PORTF_DATA_R & 0x10) == 0) {   // SW1 pressed (PF0 low)
+                    GPIO_PORTF_DATA_R =0x04;
+                }
+
+        else{
+            GPIO_PORTF_DATA_R =0x00;
+        }
+    }
+}
+void delayMs(int n) {
+    volatile int i, j;
+    for(i = 0; i < n; i++)
+        for(j = 0; j < 3180; j++);  // Approx 1 ms delay
+}
