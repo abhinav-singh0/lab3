a473785f90463df2fb7499e836ad18ccc81f4d2b
Abhinav Singh <ee25mt010@iitdh.ac.in>
2025-09-02 15:47:59 +0530

    lab04a

--- main.c
+++ main.c
@@ -1,31 +1,36 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
 
+#define GPIO_PORTF_DATA_R   (*((volatile uint32_t *)0x400253FC))
+
+// LEDs
+#define RED_LED   (1U << 1)   // PF1
+#define BLUE_LED  (1U << 2)   // PF2
+// Switches
+#define SW2       (1U << 0)   // PF0
+#define SW1       (1U << 4)   // PF4
 int main(void) {
     SYSCTL_RCGCGPIO_R |= 0x20;               // Enable clock to GPIOF
         while ((SYSCTL_PRGPIO_R & 0x20) == 0);   // Wait until ready
 
-        GPIO_PORTF_LOCK_R = 0x4C4F434B;          // Unlock commit register
-        GPIO_PORTF_CR_R |= 0x1F;                 // Allow changes to PF0–PF4
-        GPIO_PORTF_DIR_R |= 0x0E;                // PF1, PF2, PF3 = output (LEDs)
-        GPIO_PORTF_DEN_R |= 0x1F;                // Enable digital for PF0–PF4
-        GPIO_PORTF_PUR_R |= 0x11;                // Enable pull-ups on PF0, PF4 (switches)
+        GPIO_PORTF_LOCK_R = 0x4C4F434B;
+           GPIO_PORTF_CR_R |= (RED_LED | BLUE_LED | SW1 | SW2);
 
-    while(1) {
-        if((GPIO_PORTF_DATA_R & 0x01) == 0) {   // SW2 pressed (PF0 low)
-            GPIO_PORTF_DATA_R =0x02;
-        }
-        if((GPIO_PORTF_DATA_R & 0x10) == 0) {   // SW1 pressed (PF0 low)
-                    GPIO_PORTF_DATA_R =0x04;
-                }
 
-        else{
-            GPIO_PORTF_DATA_R =0x00;
-        }
-    }
-}
-void delayMs(int n) {
-    volatile int i, j;
-    for(i = 0; i < n; i++)
-        for(j = 0; j < 3180; j++);  // Approx 1 ms delay
-}
+           GPIO_PORTF_DIR_R |= (RED_LED | BLUE_LED); // PF1, PF2 = output (LEDs)
+           GPIO_PORTF_DIR_R &= ~(SW1 | SW2);         // PF0, PF4 = input (switches)
+           GPIO_PORTF_DEN_R |= (RED_LED | BLUE_LED | SW1 | SW2);
+           GPIO_PORTF_PUR_R |= (SW1 | SW2);
+
+           while(1) {
+               // SW1 pressed  turn ON Red LED
+               if((GPIO_PORTF_DATA_R & SW1) == 0) {
+                   GPIO_PORTF_DATA_R |= RED_LED;
+               }
+               if((GPIO_PORTF_DATA_R & SW2) == 0) {
+                   GPIO_PORTF_DATA_R |= BLUE_LED;
+               } else {
+                   GPIO_PORTF_DATA_R= 0x00;
+               }
+           }
+       }
